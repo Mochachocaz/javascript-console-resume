92415f6c20accd1660c211386ca98bea32e8590e
Sam Martin <sammartin1357@gmail.com>
2024-04-03 16:59:45 +0800

    added lines to javascript

--- javascript.js
+++ javascript.js
@@ -2,4 +2,11 @@ let fullname = "Sam Martin".toUpperCase();
 
 console.log("Name: " + fullname);
 console.log("Career: Digital Growth Manager");
-console.log("Description: I like video games")
+console.log("Description: I like video games, coding and drinking whiskey");
+
+console.log("* Whiskey");
+console.log("* Video Games");
+console.log("* TV Shows");
+console.log("* Hanging out with friends");
+console.log("* Practicing coding & starting side hustles");
+
